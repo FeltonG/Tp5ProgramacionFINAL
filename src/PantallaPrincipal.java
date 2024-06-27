410475a0c7e1642dcbf2205d211ab2766ce99c24
Felipe <feli2003garcia@gmail.com>
2024-06-27 15:24:02 -0300

    primercommitpicante

--- src/PantallaPrincipal.java
+++ src/PantallaPrincipal.java
@@ -4,9 +4,13 @@ public class PantallaPrincipal {
     private JLabel titulo;
     private JTextField textField1;
     private JTextField textField2;
-    private JButton magiaButton;
+    private JButton btnEnter;
     private JTextField textField3;
-    private JPanel FondoPantalla;
+    private JPanel fondoPantalla;
+    private JButton btBorrarTodo;
+    private JLabel txtVariable;
+    private JLabel TxtExpresion;
+    private JLabel txtResultado;
 
     private void createUIComponents() {
 
