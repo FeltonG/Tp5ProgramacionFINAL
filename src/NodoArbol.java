4e9bd895198e4b0f5c10df069d4f0d6fd4c8cf8c
Felipe <feli2003garcia@gmail.com>
2024-06-27 15:08:45 -0300

    alenchuhacealgo

--- src/NodoArbol.java
+++ src/NodoArbol.java
@@ -1,14 +1,32 @@
-import java.lang.foreign.ValueLayout;
-
 public class NodoArbol {
-    String valor;
+    int valor;
     NodoArbol derecha;
     NodoArbol izquierda;
 
-    public NodoArbol(String valor){
+    public NodoArbol(int valor){
         this.valor= valor;
         this.derecha=null;
         this.izquierda=null;
 
     }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public NodoArbol getDerecha() {
+        return derecha;
+    }
+
+
+    public NodoArbol getIzquierda() {
+        return izquierda;
+    }
+
+
+
 }
---
+++ src/PantallaPrincipal.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class PantallaPrincipal {
+    private JLabel titulo;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton magiaButton;
+    private JTextField textField3;
+    private JPanel FondoPantalla;
+
+    private void createUIComponents() {
+
+    }
+}
