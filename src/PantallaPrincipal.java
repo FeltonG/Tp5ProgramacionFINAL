cb2e4f7c7b995815415cdef5fb4ac0a27b85a641
Felipe <feli2003garcia@gmail.com>
2024-06-27 20:42:57 -0300

    nashe

--- src/PantallaPrincipal.java
+++ src/PantallaPrincipal.java
@@ -2,13 +2,11 @@ import javax.swing.*;
 
 public class PantallaPrincipal {
     private JLabel titulo;
-    private JTextField inputVariable;
     private JTextField inputExpresion;
     private JButton btnEnter;
     private JTextField inputResultado;
     private JPanel fondoPantalla;
     private JButton btBorrarTodo;
-    private JLabel txtVariable;
     private JLabel txtExpresion;
     private JLabel txtResultado;
 
